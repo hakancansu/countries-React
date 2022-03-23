04ebb5dde3aa2313c4af5ef77d57869c48aceaf7
hasalica <83381777+hakancansu@users.noreply.github.com>
2022-03-24 00:27:52 +0300

    hooks example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [countrys, setCountry] = useState([]);
+  const [serch, setSearch] = useState("");
+  const handleChange = (event) => setSearch(event.target.value);
+
+  useEffect(() => {
+    // fetch("https://restcountries.com/v2/all")
+    // .then(response =>response.json())
+    // .then(response=>setCountry(response));
+    axios
+      .get("https://restcountries.com/v2/all")
+      .then((response) => setCountry(response.data))
+      .catch((error) => console.log({ error }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center", color: "#8ec3c9" }}>ulkeler</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <input
+          name="serch"
+          placeholder="serach.."
+          value={serch}
+          onChange={handleChange}
+          style={{ border: "1px solid #8ec3c9" }}
+          type="text"
+        ></input>
+      </div>
+
+      {countrys
+        .filter((country) =>
+          country.name.toLowerCase().includes(serch.toLowerCase())
+        )
+        .map((country) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                border: "1px solid #8ec3c9",
+                margin: "10px",
+                padding: "5px",
+              }}
+            >
+              <img
+                style={{ width: "100px" }}
+                src={country.flag}
+                alt="bayrak"
+              ></img>
+              <div style={{ padding: "5px" }}>
+                <h2>{country.name}</h2>
+                <h4>{country.capital}</h4>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
